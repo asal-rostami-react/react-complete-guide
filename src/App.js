2110d672a80495e4fb2ef8aff469fe0058e206b5
asal <asal.rostami1984@gmail.com>
2020-01-07 15:46:58 -0500

    added a button

--- src/App.js
+++ src/App.js
@@ -4,12 +4,34 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+
+  state = {
+    persons: [
+      { name: 'Asal', age: 35},
+      { name: 'Siamak', age: 28},
+      { name: 'Julie', age: 9}
+    ]
+
+  };
+
+  switchNameHandler = () => {
+    console.log("was clicked!");
+  }
+
   render() {
     return (
       <div className="App">
        <h1>hello asal joon!!!</h1>
        <p>This is really working!</p>
-       <Person />
+       <button onClick={this.switchNameHandler}>Switch Name</button>
+
+       <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+       <Person ame={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: watchin TV</Person>
+       <Person ame={this.state.persons[2].name} age={this.state.persons[2].age}/>
+
+       {/* <Person name="Asal" age="35"/>
+       <Person name="Siamak" age="28">My Hobbies: watchin TV</Person>
+       <Person name="Julie" age="9"/> */}
       </div>
     );
     // return React.createElement('div',{className: 'App'}, React.createElement('h1', null,'Hello asal joon!!! by jsx'))
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,8 +1,19 @@
 import React from 'react';
 
+const person = (props) => {
+    return (
+        <div>
+            <p>I'm {props.name} and I am {props.age} years old.</p>
+            <p>{props.children}</p>
 
-const person = () => {
-    return <p>I'm a Person!</p>
+        </div>
+    )
 };
+
+
+
+// const person = () => {
+// return <p>I'm a Person and I am {Math.floor(Math.random() * 30)} years old.</p>
+// };
 
 export default person;
