d6d53aefe987800040e3791c8bb427df82c8c482
asal <asal.rostami1984@gmail.com>
2020-01-07 10:28:26 -0500

    added new js file called Person

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       <h1>hello asal</h1>
+       <h1>hello asal joon!!!</h1>
+       <p>This is really working!</p>
       </div>
     );
+    // return React.createElement('div',{className: 'App'}, React.createElement('h1', null,'Hello asal joon!!! by jsx'))
   }
 }
 
