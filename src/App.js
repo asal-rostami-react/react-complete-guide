2f2b5647cc330ae1c24dccf1c052b6971b041208
asal <asal.rostami1984@gmail.com>
2020-01-08 09:34:53 -0500

    passed different propeerty in onClick event

--- src/App.js
+++ src/App.js
@@ -15,11 +15,11 @@ class App extends Component {
 
   };
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     // console.log("was clicked!");
     this.setState( {
       persons: [
-        { name: 'Mina', age: 35},
+        { name: newName, age: 35},
         { name: 'Siamak', age: 36},
         { name: 'Julie', age: 9}
       ]
@@ -31,11 +31,18 @@ class App extends Component {
       <div className="App">
        <h1>hello asal joon!!!</h1>
        <p>This is really working!</p>
-       <button onClick={this.switchNameHandler}>Switch Name</button>
+       <button onClick={this.switchNameHandler.bind(this, 'Alireza')}>Switch Name</button>
 
-       <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-       <Person ame={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: watchin TV</Person>
-       <Person ame={this.state.persons[2].name} age={this.state.persons[2].age}/>
+       <Person 
+       name={this.state.persons[0].name} 
+       age={this.state.persons[0].age}/>
+       <Person 
+       name={this.state.persons[1].name} 
+       age={this.state.persons[1].age}
+       click={this.switchNameHandler.bind(this, 'Mina')} >My Hobbies: watchin TV</Person>
+       <Person 
+       name={this.state.persons[2].name} 
+       age={this.state.persons[2].age}/>
 
        {/* <Person name="Asal" age="35"/>
        <Person name="Siamak" age="28">My Hobbies: watchin TV</Person>
