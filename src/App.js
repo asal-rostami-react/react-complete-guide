ad3b1c1ef55d033c221d7bcf3167688575b758a3
asal <asal.react@gmail.com>
2020-01-10 10:50:44 -0500

    clearified the conditional part by map

--- src/App.js
+++ src/App.js
@@ -57,19 +57,14 @@ class App extends Component {
     if( this.state.showPerson ) {
       persons = (
         <div>
-              <Person 
-              name={this.state.persons[0].name} 
-              age={this.state.persons[0].age}/>
-              <Person 
-              name={this.state.persons[1].name} 
-              age={this.state.persons[1].age}
-              click={this.switchNameHandler.bind(this, 'Mina')} 
-              changed={this.nameChangedHandler}>My Hobbies: watchin TV</Person>
-              <Person 
-              name={this.state.persons[2].name} 
-              age={this.state.persons[2].age}/>
-
-          </div>
+          {
+            this.state.persons.map(peron => {
+              return <Person
+              name={peron.name}
+              age={peron.age}  />
+            })
+          }
+        </div>
 
       );
 
