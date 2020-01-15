31dfb65f7a56f877846607278dea70d2f64a736e
asal <asal.react@gmail.com>
2020-01-15 16:59:11 -0500

    added container folder and added App files in it

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 import classes from './App.css';
-import Person from './Person/Person';
+import Person from '../components/Persons/Person/Person';
 
 // import Radium, {StyleRoot} from 'radium';
 // import styled from 'styled-components';
