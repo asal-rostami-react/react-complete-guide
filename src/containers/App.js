e56eddf57d25f1e370377b784e4f44ad56d9c11e
asal <asal.react@gmail.com>
2020-01-16 10:01:55 -0500

    changed Person to  Persons

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 import classes from './App.css';
-import Person from '../components/Persons/Person/Person';
+import Persons from '../components/Persons/Persons';
 
 // import Radium, {StyleRoot} from 'radium';
 // import styled from 'styled-components';
@@ -65,17 +65,10 @@ class App extends Component {
     if( this.state.showPerson ) {
       persons = (
         <div>
-          {
-            this.state.persons.map((person, index) => {
-              return <Person
-                  click={() => this.deletePersonHandler(index)}
-                  name={person.name}
-                  age={person.age} 
-                  key={person.id}
-                  changed={(event) => this.nameChangedHandler(event, person.id)} />
-              
-            })
-          }
+          <Persons 
+          persons={this.state.persons}
+          clicked={this.deletePersonHandler}
+          changed={this.nameChangedHandler}/>
         </div>
 
       );
