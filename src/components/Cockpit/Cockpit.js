2a85d9c86e92db3f0b61cde48889fff50a0eed9a
asal <asal.react@gmail.com>
2020-01-16 14:07:59 -0500

    sent a title as a props in App.js and used it

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -14,13 +14,13 @@ const cockpit = (props) => {
      if(props.persons.length <= 2){
        assignedClasses.push(classes.red);
      }
-     
+
      if(props.persons.length <= 1){
        assignedClasses.push(classes.bold);
      }
     return(
         <div className={classes.Cockpit}>
-            <h1>hello asal joon!!!</h1>
+            <h1>{props.title}</h1>
             <p className={assignedClasses.join(' ')}>This is really working!</p>
             <button className={btnClass} onClick={props.clicked}>
             Toggle Persons
