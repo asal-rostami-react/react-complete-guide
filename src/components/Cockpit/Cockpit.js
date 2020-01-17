12e133b1e4ca103637e21837ed4caa283f831013
asal <asal.react@gmail.com>
2020-01-17 10:18:51 -0500

    added useEffect

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,32 +1,41 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import classes from './Cockpit.css';
 
 const cockpit = (props) => {
-     // let classes = ['red', 'bold'].join(' ');
-     const assignedClasses = [];
-     let btnClass = '';
+  useEffect(() =>{ 
+    console.log('[cockpit.js] useEffect');
+    //Http request....
+    // like
+    setTimeout(() => {
+      console.log("Saved data to cloud!");
+    }, 1000);
+  } ,[props.persons]);
+  
+    // let classes = ['red', 'bold'].join(' ');
+    const assignedClasses = [];
+    let btnClass = '';
 
-     if(props.showPersons)
-     {
-        btnClass =classes.Red;
-     }
-      
-     if(props.persons.length <= 2){
-       assignedClasses.push(classes.red);
-     }
+    if(props.showPersons)
+    {
+      btnClass =classes.Red;
+    }
+    
+    if(props.persons.length <= 2){
+      assignedClasses.push(classes.red);
+    }
 
-     if(props.persons.length <= 1){
-       assignedClasses.push(classes.bold);
-     }
-    return(
-        <div className={classes.Cockpit}>
-            <h1>{props.title}</h1>
-            <p className={assignedClasses.join(' ')}>This is really working!</p>
-            <button className={btnClass} onClick={props.clicked}>
-            Toggle Persons
-            </button>
-        </div>   
-    );
+    if(props.persons.length <= 1){
+      assignedClasses.push(classes.bold);
+    }
+  return(
+      <div className={classes.Cockpit}>
+          <h1>{props.title}</h1>
+          <p className={assignedClasses.join(' ')}>This is really working!</p>
+          <button className={btnClass} onClick={props.clicked}>
+          Toggle Persons
+          </button>
+      </div>   
+  );
 }
 
 export default cockpit;
