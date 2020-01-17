7603ea07c61987fbad4fda0b54b4e21aaf4288dd
asal <asal.react@gmail.com>
2020-01-17 10:36:26 -0500

    added class-based component of persons.js

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -15,3 +15,61 @@ const persons = (props) => {
 };
 
 export default persons;
+
+
+// Turning persons functional component into class-based component in order to use lifecycle functions
+
+// import React, { Component } from 'react';
+
+// import Person from './Person/Person';
+
+// class Persons extends Component {
+//   // static getDerivedStateFromProps(props, state) {
+//   //   console.log('[Persons.js] getDerivedStateFromProps');
+//   //   return state;
+//   // }
+
+//   // componentWillReceiveProps(props) {
+//   //   console.log('[Persons.js] componentWillReceiveProps', props);
+//   // }
+
+//   shouldComponentUpdate(nextProps, nextState) {
+//     console.log('[Persons.js] shouldComponentUpdate');
+//     return true;
+//   }
+
+//   getSnapshotBeforeUpdate(prevProps, prevState) {
+//     console.log('[Persons.js] getSnapshotBeforeUpdate');
+//     return { message: 'Snapshot!' };
+//   }
+
+//   // componentWillUpdate() {
+
+//   // }
+
+
+//   componentDidUpdate(prevProps, prevState, snapshot) {
+//     console.log('[Persons.js] componentDidUpdate');
+//     console.log(snapshot);
+//   }
+  // componetWillUnmount(){
+  //   console.log('[Persons.js] componentWillUnmount');
+  // }
+
+//   render() {
+//     console.log('[Persons.js] rendering...');
+//     return this.props.persons.map((person, index) => {
+//       return (
+//         <Person
+//           click={() => this.props.clicked(index)}
+//           name={person.name}
+//           age={person.age}
+//           key={person.id}
+//           changed={event => this.props.changed(event, person.id)}
+//         />
+//       );
+//     });
+//   }
+// }
+
+// export default Persons;
