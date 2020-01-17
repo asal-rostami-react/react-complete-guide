078fd0232bcfa97e1aad161f475f7619649ffba1
asal <asal.react@gmail.com>
2020-01-17 13:31:10 -0500

    modified   shouldComponentUpdate

--- src/containers/App.js
+++ src/containers/App.js
@@ -33,12 +33,14 @@ class App extends Component {
     console.log('[App.js] componentDidMount');
   }
   shouldComponentUpdate(nextProps, nextState) {
-    console.log('[App.js] shouldComponentUpdate');
-    if(nextProps.persons !== this.props.persons){
-      return true;
-    } else {
-      return false;
-    }
+    // console.log('[App.js] shouldComponentUpdate');
+    // if(nextProps.persons !== this.props.persons){
+    //   return true;
+    // } else {
+    //   return false;
+    // }
+
+    return true;
     
   }
 
