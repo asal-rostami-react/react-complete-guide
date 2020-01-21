2d562b238971b1e737bf14afab9911a6a0d22d96
asal <asal.react@gmail.com>
2020-01-21 10:02:41 -0500

    saved changes

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -5,7 +5,7 @@ import classes from './Person.css';
 const person = (props) => {
     return (
         // <div className={classes.Person}> 
-        <Aux className={classes.Person}>
+        <Aux>
             <p onClick={props.clicked}>I'm {props.name} and I am {props.age} years old.</p>
             <p>{props.children}</p>
             <input type="text" onChange = {props.changed} value={props.name}></input>
