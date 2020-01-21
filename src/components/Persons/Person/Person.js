047b81e7deba631dfe4aff39d3d3b088702a9932
asal <asal.react@gmail.com>
2020-01-21 15:19:44 -0500

    added class-based form to person

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -20,9 +20,6 @@ const person = (props) => {
 
     return (
         // <div className={classes.Person}> 
-        // <React.Fragment> 
-        
-      // <Aux classes={classes.Person}>
         <Aux>
             <p onClick={props.clicked}>I'm {props.name} and I am {props.age} years old.</p>
             <p key="i2">{props.children}</p>
@@ -33,7 +30,6 @@ const person = (props) => {
             value={props.name}></input>
 
         </Aux> 
-        // </React.Fragment>
         // </div>
     )
 };
@@ -50,3 +46,56 @@ person.propTypes = {
 export default withClass(person, classes.Person);
 
 // export default person;
+
+
+// *******************************************************************************************
+// class-based Person component
+// ******************************************************************************************
+
+// import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
+
+// import Aux from '../../../hoc/Aux';
+// import withClass from '../../../hoc/withClass';
+// import classes from './Person.css';
+
+// class Person extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.inputElementRef = React.createRef();
+//   }
+
+//   componentDidMount() {
+//     // this.inputElement.focus();
+//     this.inputElementRef.current.focus();
+//   }
+
+//   render() {
+//     console.log('[Person.js] rendering...');
+//     return (
+//       <Aux>
+//         <p onClick={this.props.click}>
+//           I'm {this.props.name} and I am {this.props.age} years old!
+//         </p>
+//         <p key="i2">{this.props.children}</p>
+//         <input
+//           key="i3"
+//           // ref={(inputEl) => {this.inputElement = inputEl}}
+//           ref={this.inputElementRef}
+//           type="text"
+//           onChange={this.props.changed}
+//           value={this.props.name}
+//         />
+//       </Aux>
+//     );
+//   }
+// }
+
+// Person.propTypes = {
+//   click: PropTypes.func,
+//   name: PropTypes.string,
+//   age: PropTypes.number,
+//   changed: PropTypes.func
+// };
+
+// export default withClass(Person, classes.Person);
