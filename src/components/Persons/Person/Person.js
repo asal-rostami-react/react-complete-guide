99b1f9501a960d22614ff6d44e63d2e770b54378
asal <asal.react@gmail.com>
2020-01-21 10:37:04 -0500

    passed Person.css as a class to Aux.js

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -6,7 +6,7 @@ const person = (props) => {
     return (
         // <div className={classes.Person}> 
         // <React.Fragment> 
-        <Aux>
+        <Aux classes={classes.Person}>
             <p onClick={props.clicked}>I'm {props.name} and I am {props.age} years old.</p>
             <p>{props.children}</p>
             <input type="text" onChange = {props.changed} value={props.name}></input>
--- src/hoc/Aux.js
+++ src/hoc/Aux.js
@@ -1,3 +1,8 @@
-const aux = props => props.children;
+import React from 'react';
+
+// const aux = props => props.children;
+const aux = props => (
+    <div className={props.classes}>{props.children}</div>
+); 
 
 export default aux;
